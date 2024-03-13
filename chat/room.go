54ffb3a561b025e328218d37db1ca1d289ff6161
Yerdaulet <ye.batalov>
2024-03-13 18:05:15 +0500

    chat: add room's ServeHTTP method and room constructor
    Signed-off-by: Yerdaulet <ye.batalov>

--- chat/room.go
+++ chat/room.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize,
+	WriteBufferSize: socketBufferSize}
+
 type room struct {
 	// forward is a channel that holds incoming messages
 	// that should be forwarded to the other clients.
@@ -10,6 +25,16 @@ type room struct {
 	leave chan *client
 	// clients holds all current clients in this room.
 	clients map[*client]bool
+}
+
+// newRoom makes a new room.
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+	}
 }
 
 func (r *room) run() {
@@ -29,4 +54,21 @@ func (r *room) run() {
 			}
 		}
 	}
+}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
 }
